c9af775c45aace05766e4b32efbd9fe5a915a9c5
joaquinmenendez <jm622@duke.edu>
2020-04-17 18:50:54 +0000

    added function to save the fig

--- draft.py
+++ draft.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from imutils import  url_to_image, opencv2matplotlib
 #from hof.face_detectors import RfcnResnet101FaceDetector, SSDMobileNetV1FaceDetector,FasterRCNNFaceDetector, YOLOv2FaceDetector, TinyYOLOFaceDetector
-import face_recognition
+
 
 
 MIN_CONFIDENCE=0.5
@@ -25,8 +25,10 @@ def cropFace(path_to_img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     mtcnn = MTCNN(select_largest=False, post_process = False, margin = 50)
     img_cropped = mtcnn(img)
-    plt.imshow(img_cropped.permute(1, 2, 0).int().numpy())
+    plt.imshow(img_cropped.permute(1, 2, 0).int().numpy() )  
     plt.axis('off')
+    plt.savefig('multimedia/messi_face.jpg')
+    print('Done')
 
 
 cropFace("./multimedia/messi.jpg")
