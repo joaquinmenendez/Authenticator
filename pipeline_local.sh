f9c650a26bb9876d85d283a1a905cbd924c56240
Joaquin Menendez <jm622@duke.edu>
2020-04-22 03:48:07 +0000

    Added an optional argument ROTATE. By default is ROTATE_90_CLOCKWISE

--- pipeline_local.sh
+++ pipeline_local.sh
@@ -3,6 +3,8 @@
 # 2 : Video file path
 # 3 : [ROTATE_90_CLOCKWISE, ROTATE_180, ROTATE_90_COUNTERCLOCKWISE] -- default=ROTATE_90_CLOCKWISE
 
+ROTATE=${3:-ROTATE_90_CLOCKWISE} #set 90 degress as the default rotation (Apple)
+
 echo Processing: $1
 
 #Check if the directory exist. If it does not then create it
@@ -23,6 +25,6 @@ else
     cp $2 $1/video/$ext #copy the video  from original path with the same name
 fi
 
-
-python3 video2frame.py $2 --output_file $1/images --name $1 --rotate $3 # Transform the video into frames
+#Run python scripts
+python3 video2frame.py $2 --output_file $1/images --name $1 --rotate $ROTATE # Transform the video into frames
 python3 cropAllFaces.py $1/images $1/faces #Crop oly the faces from the images
