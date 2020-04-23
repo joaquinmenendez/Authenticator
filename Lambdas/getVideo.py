df14aeaec83f46ab6815021be00c76c8b5da9729
Joaquin Menendez <43391630+joaquinmenendez@users.noreply.github.com>
2020-04-23 20:35:01 +0000

    updated last version. It stores the frames a jpg files

--- Lambdas/getVideo.py
+++ Lambdas/getVideo.py
@@ -32,19 +32,27 @@ def get_video(event):
 	
 	
 def video2frame(s3_client, video_path , bucket_name, person_name, mod_num, rotate ):
-	video = cv2.VideoCapture(video_path)
-	video_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-	frames = frame_list(video, video_len, mod_num)
-	# Iterate over frames and store every frame a an individual object
-	total_files = len(frames)
-	for n,frame in enumerate(frames):
-		p_name =  f"{person_name}/Frames/{person_name}_{n}.pickle" #it's saving as an object every frame
-		my_array_data = io.BytesIO()   #binarize into a binary file
-		pickle.dump(frame, my_array_data)
-		my_array_data.seek(0)
-		s3_client.upload_fileobj(my_array_data, bucket_name, p_name)
-		print(f'{p_name} has been succesfully uploaded. {n+1} of {total_files} remaining')
-	
+    video = cv2.VideoCapture(video_path)
+    video_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    frames = frame_list(video, video_len, mod_num)
+    # Iterate over frames and store every frame a an individual object
+    try:
+    	os. rmdir('/tmp/frames')
+    except: pass
+    os.mkdir('/tmp/frames')
+    print('directory made') 
+    [cv2.imwrite(os.path.join('/tmp/frames','{name}_{num}.jpg'.format(name=person_name, num=str(i))), cv2.rotate(frame, eval("cv2." + rotate) ))
+    for i,frame in enumerate(frames,1)] 
+    print("{0} images saved in temp/frames/{1}".format(len(frames),person_name))
+    len_files = len(os.listdir('/tmp/frames/')) #how many files
+    print(f'Uploading {len_files} files to {bucket_name}/{person_name}/Frames')
+    print(os.listdir('/tmp/frames/'))
+    for file in os.listdir('/tmp/frames'):
+    	key = f'{person_name}/frames/{file}'
+    	s3_client.upload_file(os.path.join('/tmp/frames',file), bucket_name, key) 
+    print ('Done!')
+
+
 def frame_list(video, video_len, mod_num):
 	"""
 	"""
