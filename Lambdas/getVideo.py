4a2a0be2f24f97ceb451b97bd3a479ae25a9e4dd
Joaquin Menendez <43391630+joaquinmenendez@users.noreply.github.com>
2020-04-23 19:54:30 +0000

    Modified some tab spaces

--- Lambdas/getVideo.py
+++ Lambdas/getVideo.py
@@ -3,7 +3,6 @@
 ## Memory 320 MB
 ## Runtime 1 minute
 ## Layers: arn:aws:lambda:us-east-1:177627986553:layer:cv2-boto3:1
-
 import boto3
 import os
 import pickle
@@ -33,28 +32,25 @@ def get_video(event):
 	
 	
 def video2frame(s3_client, video_path , bucket_name, person_name, mod_num, rotate ):
-  """
-  """
-  video = cv2.VideoCapture(video_path)
-  video_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-  frames = frame_list(video, video_len, mod_num)
-  # Iterate over frames and store every frame a an individual object
-  total_files = len(frames)
-  for n,frame in enumerate(frames):
-    p_name =  f"{person_name}/{person_name}_{n}.pickle" #it's saving as an object every frame
-    my_array_data = io.BytesIO()   #binarize into a binary file
-    pickle.dump(frame, my_array_data)
-    my_array_data.seek(0)
-    s3_client.upload_fileobj(my_array_data, bucket_name, p_name)
-    print(f'{p_name} has been succesfully uploaded. {n} of {total_files} remaining')
-  print('Done!')
+	video = cv2.VideoCapture(video_path)
+	video_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+	frames = frame_list(video, video_len, mod_num)
+	# Iterate over frames and store every frame a an individual object
+	total_files = len(frames)
+	for n,frame in enumerate(frames):
+		p_name =  f"{person_name}/Frames/{person_name}_{n}.pickle" #it's saving as an object every frame
+		my_array_data = io.BytesIO()   #binarize into a binary file
+		pickle.dump(frame, my_array_data)
+		my_array_data.seek(0)
+		s3_client.upload_fileobj(my_array_data, bucket_name, p_name)
+		print(f'{p_name} has been succesfully uploaded. {n+1} of {total_files} remaining')
 	
 def frame_list(video, video_len, mod_num):
 	"""
 	"""
 	frames = []
 	for i in range(video_len):
-	 	# Load frame
+		# Load frame
 		success = video.grab()
 		if i % mod_num == 0:
 			success, frame = video.retrieve()
@@ -63,7 +59,6 @@ def frame_list(video, video_len, mod_num):
 		if not success:
 			continue
 		# Add to batch
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	frames.append(frame)
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		frames.append(frame)
 	return frames
-  
