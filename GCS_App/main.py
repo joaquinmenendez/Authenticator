3ca071243b0b28bd852a34bfa195a07249e37e32
joaquin <jm622@duke.edu>
2020-04-26 15:50:20 -0400

    added preProcessPhoto to the import

--- GCS_App/main.py
+++ GCS_App/main.py
@@ -2,8 +2,8 @@ import pandas as pd
 from flask import Flask, render_template, request, flash, redirect 
 import os
 from werkzeug.utils import secure_filename
-from embeddingFaces import embeddings
-from cropFace import cropFace
+from preProcessPhoto import preProcessPhoto
+
 
 
 #Initialize variables
@@ -23,21 +23,6 @@ def allowed_file(filename):
     '''
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-def preProcessPhoto(file_path):
-    '''
-    The function tooks a photo,  crops the face and get the embeddings for that face using resnet.
-    :arguments:
-        file_path (string) : The path where the image is stored
-    :return:
-        j_embedding (JSON object): A JSON object with the following structure: {"body" : [embedding numpy array]}
-    '''
-
-    name_file = file_path.split('/')[-1]  # Get the name of the file
-    print(name_file)
-    cropFace(file_path, output_img = f'tmp/Cropped_{name_file}')  # Crop the face
-    j_embedding = embeddings(f'tmp/Cropped_{name_file}', tensor=False)  # Get the embedding of the face
-    return j_embedding
 
 def GET_probability(url, j_embedding):
     '''
