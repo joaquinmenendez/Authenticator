1bc4d81745deb27152ea63fdad508a0a7e2906d4
joaquin <jm622@duke.edu>
2020-04-26 15:51:51 -0400

    Function that took an image's path, preprocess the photo and return a json with the face embedding

---
+++ GCS_App/preProcessPhoto.py
@@ -0,0 +1,30 @@
+import argparse
+from embeddingFaces import embeddings
+from cropFace import cropFace
+
+parser = argparse.ArgumentParser()  # Parser for command-line options
+parser.add_argument("file_path", help="Directory file of the image preprocess", type=str)
+
+def preProcessPhoto(file_path):
+    '''
+    The function tooks a photo,  crops the face and get the embeddings for that face using resnet.
+    :arguments:
+        file_path (string) : The path where the image is stored
+    :return:
+        j_embedding (JSON object): A JSON object with the following structure: {"body" : [embedding numpy array]}
+    '''
+
+    name_file = file_path.split('/')[-1]  # Get the name of the file
+    print(name_file)
+    cropFace(file_path, output_img = f'tmp/Cropped_{name_file}')  # Crop the face
+    j_embedding = embeddings(f'tmp/Cropped_{name_file}', tensor=False)  # Get the embedding of the face
+    return j_embedding
+
+
+def main():
+    args = parser.parse_args()
+    preProcessPhoto(args.file_path)
+
+
+if __name__ == '__main__':
+    main()
