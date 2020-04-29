4daaddfc200f8b51cd1cb06626d65abad485be4f
AzucenaMV <azucena.mova@mgmail.com>
2020-04-29 21:21:41 +0000

    Fixing some minor errors

--- GCS_App/train_deploy_model.py
+++ GCS_App/train_deploy_model.py
@@ -3,35 +3,36 @@ import sagemaker
 from sagemaker import get_execution_role
 import boto3
 import argparse
-import json
+
+
 
 parser = argparse.ArgumentParser() # Parser for command-line options
 parser.add_argument("keys", help = "Name of the bucket to download from", type = str)
 parser.add_argument("--instance", help = "Name of the video inside the bucket", type = str)
 parser.add_argument("--instance_count", help = "Directory to store the video", type = int)
-parser.add_argument("--endpoint_name", help = "Name of the endpoint in which to deploy the model", type = str) 
+parser.add_argument("--update", help = "Update should be true if there is the endpoint is already open", type = bool) 
 parser.add_argument("--model_path", help = "File with access keys", type = str) 
 parser.add_argument("--hyperparms", help = "Hyperparameters for SVM", type = str) # Default is None
-parser.add_argument("--bucket_name", help = "Bucket in which the data is stored", type = str)
 parser.add_argument("--key_bucket", help = "Key of the pickle data with the data", type = str) 
 
 def train_deploy_model(keys,
                   instance = 'ml.m4.xlarge', # Don't change this!
                   instance_count = 1, # Don't change this!
-                  endpoint_name = "SVM-image-classifier-2020-04",
                   model_path = '/content/model/model.py',
-                  bucket_name = 'video-facerecogproj',
                   key_bucket = 'data/data.pickle',
+                  update = True, # This should be always true if there is an open endpoint
                   hyperparms = None):
   """
   This function trains a sagemaker model and deploys it.
 
     Args:
        keys (json): Json with credential keys
-       instance (str): instance type to train model and deploy it
-       instance_count (int): initial instance count for deploying the model
+       instance (str): Instance type to train model and deploy it
+       instance_count (int): Initial instance count for deploying the model
+       update (bool): Update should be true if there is the endpoint is already open
        model_path (str): Directory path where the model is located
        hyperparms (dictionary): Hyperparameters for SVM
+       key_buket (str): Key of the pickle data with the data
     
     Returns:
        Print statement
@@ -50,27 +51,29 @@ def train_deploy_model(keys,
   sklearn = SKLearn(
       entry_point = model_path,
       train_instance_type= instance,
-      #train_instance_type='local',
-      role = keys['ROLE'],
+      role = keys["ROLE"],
       sagemaker_session=sagemaker_session,
       hyperparameters= hyperparms
   )
   ## Data for training 
-  inputs = sagemaker_session.upload_data(path='data', key_prefix=key_bucket, bucket=bucket_name)
+  inputs = sagemaker_session.upload_data(path='data', key_prefix=key_bucket, bucket=keys["BUCKET_NAME"])
   ## Training the model
   sklearn.fit({'train': inputs})
   ## Deploying the model
-  predictor = sklearn.deploy(initial_instance_count = instance_count,
-                            instance_type= instance,
-                            endpoint_name = endpoint_name,
-                            update_endpoint = True
-                                )
+  try:
+    predictor = sklearn.deploy(initial_instance_count = instance_count,
+                              instance_type= instance,
+                              endpoint_name = keys[endpoint_name],
+                              update_endpoint = update
+                                  )
+  except:
+    print("The model was not deployed")
+  
   return print("Endpoint updated: {}".format(endpoint_name))
 
 def main():
     args = parser.parse_args()
-    train_deploy_model(args.keys,args.instance,args.instance_count,
-                       args.endpoint_name,args.model_path,args.bucket_name,
+    train_deploy_model(args.keys,args.instance,args.instance_count,args.model_path,
                        args.key_bucket,args.hyperparms)
 
 if __name__ == "__main__":
