c4fea4807c469092a1dd4a5274dd9e7897690592
joaquin <jm622@duke.edu>
2020-04-29 22:20:40 -0400

    added color inside the function CropFace.py

--- GCS_App/preProcessPhoto.py
+++ GCS_App/preProcessPhoto.py
@@ -9,14 +9,15 @@ def preProcessPhoto(file_path, model):
     '''
     The function tooks a photo,  crops the face and get the embeddings for that face using resnet.
     :arguments:
-        file_path (string) : The path where the image is stored
+        file_path (str) : The path where the image is stored
         model (facenet model): The model that will be used to embeed the faces.
+        
     :return:
         j_embedding (JSON object): A JSON object with the following structure: {"body" : [embedding numpy array]}
     '''
     name_file = file_path.split('/')[-1]  # Get the name of the file
     print(f'Processing: {name_file}')
-    cropFace(file_path, output_img = f'tmp/Cropped_{name_file}')  # Crop the face
+    cropFace(file_path, output_img = f'tmp/Cropped_{name_file}', color = 'RGB')  # Crop the face. color (str) : BGR is default in cropFace.py. Choose 'RGB' to overwrite"
     j_embedding = embeddings(f'tmp/Cropped_{name_file}', model=model, tensor=False)  # Get the embedding of the face
     return j_embedding
 
